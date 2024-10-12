6dceb04e72e7404f43fa7447e2d1a1a856ce662d
Shezod Usmonov <sherzod.usmon.91@gmail.com>
2024-10-12 15:39:21 +0500

    added new addElementAt and addElementToHead

--- LinkedList/index.php
+++ LinkedList/index.php
@@ -7,12 +7,22 @@ $data = [12, 23, 32, 12, 2, 43, 45];
 $link = new LinkedList($data);
 
 // add element to linked list
-$link->addElementToEnd("Last element 8");
-$link->addElementToEnd("Last element 9");
+$link->addElement("Last element 8");
+$link->addElement("Last element 9");
 
 // print
 echo $link->returnAsString() . "\n";
 echo $link->getLength() . "\n";
-var_dump($link->removeByIndex(3));
+
+if ($link->removeElementAt(index: 3)) {
+    echo "deleted element index by 3 \n";
+} else {
+    echo "Not deleted element index by 3 \n";
+}
+echo $link->returnAsString() . "\n";
+echo $link->getLength() . "\n";
+
+echo $link->addElementAt(3, "Five");
+echo $link->addElementAt(3, "Four");
 echo $link->returnAsString() . "\n";
 echo $link->getLength() . "\n";
--- LinkedList/libs.php
+++ LinkedList/libs.php
@@ -19,15 +19,20 @@ class LinkedList
     public function __construct(array $data = [])
     {
         foreach($data as $value) {
-            $this->addElementToEnd($value);
+            $this->addElement($value);
         }
     }
 
-    public function addElementToEnd(mixed $value): void
+    public function addElementToHead(mixed $value): void
+    {
+        $this->head = new Node($value, $this->head);
+    }
+
+    public function addElement(mixed $value): void
     {
         // if it is first element, so just set a head
         if (is_null($this->head)) {
-            $this->head = new Node($value, null);
+            $this->addElementToHead($value);
             return;
         }
 
@@ -41,21 +46,25 @@ class LinkedList
         $node->next = new Node($value, null);
     }
 
-    public function removeByIndex(int $index): bool | Exception
+    public function removeElementAt(int $index): bool | Exception
     {
+        // throw exception invalid index
         if ($index < 0 || $index > $this->getLength()) {
             throw new Exception("The index is invalid", 100);
         }
 
+        // throw exception adding value empty liked link
         if (is_null($this->head)) {
             throw new Exception("Linked Link is empty", 100);
         }
 
+        // remove head
         if ($index == 0) {
             $this->head = $this->head->next;
             return true;
         }
 
+        // find and remove element at index
         $counter = 0;
         $node = $this->head;
         while($node) {
@@ -71,10 +80,40 @@ class LinkedList
         return true;
     }
 
+    public function addElementAt(int $index, mixed $value): void
+    {
+        // throw exception invalid index
+        if ($index < 0 || $index > $this->getLength()) {
+            throw new Exception("The index is invalid", 100);
+        }
+
+        // checking if it is empty, add element to ehad
+        if (is_null($this->head) || $index == 0) {
+            $this->addElementToHead($value);
+        }
+        // else find and add element at given index 
+        else {
+            $counter = 1;
+            $node = $this->head;
+            while($node) {
+                if ($counter == $index) {
+                    $ref = &$node;
+                    $new  = new Node($value, $node->next);
+                    $ref->next = $new;
+                    break;
+                }
+                $counter += 1;
+                $node = $node->next;
+            }
+        }
+
+    }
+
     public function getLength(): int
     {
         $counter = 0;
 
+        // calculate langth
         $node = $this->head;
         while ($node) {
             $counter += 1;
@@ -94,7 +133,7 @@ class LinkedList
         $result = "{$node->value}";
         while ($node->next) {
             $node = $node->next;
-            $result .= ",{$node->value}";
+            $result .= "-> {$node->value}";
         }
 
         return $result;
