0de14fc3bbb286580970ec20ee9b5b5f50a4fefa
Shezod Usmonov <sherzod.usmon.91@gmail.com>
2024-10-23 18:14:09 +0500

    implmented Stack class

---
+++ Stack/index.php
@@ -0,0 +1,19 @@
+<?php
+
+include_once "libs.php";
+
+$stack = new Stack();
+$stack->push(12);
+echo "push 12 value \n";
+$stack->push(13);
+echo "push 13 value \n";
+$stack->push(14);
+echo "push 14 value \n";
+echo $stack->display() . "\n";
+echo "pop last value: " . $stack->pop() . "\n";
+echo $stack->display() . "\n";
+echo "peek first value: " . $stack->peek() . "\n";
+echo $stack->display() . "\n";
+$stack->push(15);
+echo "push 15 value \n";
+echo $stack->display() . "\n";
---
+++ Stack/libs.php
@@ -0,0 +1,64 @@
+<?php
+
+interface IStack
+{
+    public function push(mixed $value): void;
+    public function pop(): mixed;
+    public function peek(): mixed;
+    public function isEmpty(): bool;
+    public function length(): int;
+    public function display(): string;
+}
+
+class Stack implements IStack
+{
+    private array $data;
+
+    public function __construct()
+    {
+        $this->data = [];
+    }
+
+    public function push(mixed $value): void
+    {
+        array_push($this->data, $value);
+    }
+
+    public function pop(): mixed
+    {
+        if ($this->isEmpty()) {
+            throw new Exception("Stack is empty", 100);
+        }
+
+        return array_pop($this->data);
+    }
+
+    public function peek(): mixed
+    {
+        if ($this->isEmpty()) {
+            throw new Exception("Stack is empty", 100);
+        }
+
+        return array_shift($this->data);
+    }
+
+    public function isEmpty(): bool
+    {
+        return $this->length() == 0;
+    }
+
+    public function length(): int
+    {
+        return count($this->data);
+    }
+
+    
+    public function display(): string
+    {
+        $result = "";
+        foreach($this->data as $value) {
+            $result .= "{$value} -> ";
+        }
+        return $result;
+    }
+}
