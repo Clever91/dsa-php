5da836aa845d28a56e16de1abd60b71efa83e2b4
Shezod Usmonov <sherzod.usmon.91@gmail.com>
2024-11-27 16:07:41 +0500

    finished

--- Algorithms/binary_search_exercises.php
+++ Algorithms/binary_search_exercises.php
@@ -9,20 +9,43 @@ include_once __DIR__ . "./binary_search.php";
  * This should return 5,6,7 as indices containing number 15 in the array
  */
 
-function findOccuranceIndexes($numbers, $lookingNumber): array
+function findOccuranceIndexes($numbers, $lookingNumber): string
 {
     $indexes = [];
+    $endIndex = count($numbers) - 1;
 
-    return $indexes;
+    $index = binarySearch($numbers, $lookingNumber);
+
+    if ($index === -1) {
+        return "";
+    }
+
+    // checking right side
+    $rightIndex = $index;
+    while($rightIndex <= $endIndex && $lookingNumber == $numbers[$rightIndex]) {
+        array_push($indexes, $rightIndex);
+        $rightIndex++;
+    }
+
+    // checking left side
+    $leftIndex = $index - 1;
+    while($leftIndex >= 0 && $lookingNumber == $numbers[$leftIndex]) {
+        array_push($indexes, $leftIndex);
+        $leftIndex--;
+    }
+
+    // make sort
+    sort($indexes);
+    return implode(", ", $indexes);
 }
 
 
 // test_001
-$numbers = [1, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7];
-$indexes = findOccuranceIndexes($numbers, 7);
-var_dump($indexes);
+// $numbers = [1, 1, 2, 2, 3, 4, 5, 6, 7, 7, 7, 7, 7];
+// $indexes = findOccuranceIndexes($numbers, 2);
+// echo $indexes;
 
 // test_002
 // $numbers = [1,4,6,9,11,15,15,15,17,21,34,34,56];
-// $indexes = findOccuranceIndexes($numbers, 15);
-// var_dump($indexes);
+// $indexes = findOccuranceIndexes($numbers, 56);
+// echo $indexes;
